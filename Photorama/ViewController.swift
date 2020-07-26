129a55824f1941015ee874549ebb6964dca15580
Guillermo Fabian Padilla Lam <gfabian.lam@gmail.com>
2020-07-26 10:40:31 -0500

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Guillermo Padilla Lam on 26/07/20.
+//  Copyright © 2020 gp Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
